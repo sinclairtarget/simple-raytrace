59fba93001b124a68a8c99c1e935f985aa39a135
Sinclair Target <sinclairtarget@gmail.com>
2017-03-17 21:51:25 -0400

    blinn-phong.

--- src/shading.c
+++ src/shading.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "shading.h"
 #include "scene.h"
 #include "macros.h"
@@ -8,12 +10,27 @@ Color Shade(Camera* camera, RayHit* hit)
     Vec3 lightDirNorm = Vec3Norm(lightDir);
     float lightIntensity = 0.7f;
 
+    // Caclulate lambertian component
     float angleCooefficient = max(0, Vec3Dot(hit->normal, lightDirNorm));
     Color lambertian = 
         ColorScale(hit->surfaceColor, lightIntensity * angleCooefficient);
 
+    // Calculate blinn-phong component
+    Vec3 camDir = Vec3Norm(Vec3Sub(camera->position, hit->point));
+    Vec3 halfVector = Vec3Norm(Vec3Add(camDir, lightDir));
+
+    float halfVectorAngleCooefficent =
+        max(0, Vec3Dot(hit->normal, halfVector));
+    halfVectorAngleCooefficent = 
+        powf(halfVectorAngleCooefficent, hit->surfacePhongExponent);
+
+    Color phong = ColorScale(hit->surfaceSpecularColor, 
+                             lightIntensity * halfVectorAngleCooefficent);
+
+    // Calculate ambient component
     Color ambient = 
         ColorScale(hit->surfaceColor, globalScene->ambientLightIntensity);
 
-    return ColorAdd(lambertian, ambient);
+    // Resulting color is sum of all components
+    return ColorAdd(ColorAdd(lambertian, phong), ambient);
 }
